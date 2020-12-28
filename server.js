d600ea0cb41ec24e10e7284d700bab8fa024501b
Jake Anstey <jake@Jakes-MacBook-Air.local>
2020-12-27 21:13:34 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+// const https = require('https');
+// const path = require('path');
+// const express = require('express');
+// const app = express();
+// const fs = require('fs')
+
+// app.use(express.static(path.join(__dirname, 'build')));
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// })
+
+// https.createServer({
+//   key: fs.readFileSync('server.key'),
+//   cert: fs.readFileSync('server.cert')}, 
+//   app).listen(443, () => {
+//   console.log('Listening...')
+// })
+
+const http = require('http');
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
+
+http.createServer(app).listen(8080, () => {
+  console.log('Listening...')
+})
