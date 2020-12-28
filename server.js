1e9f9153e550441f0e89da3209e2cd9c69f425f1
Jake Anstey <jake@Jakes-MacBook-Air.local>
2020-12-28 15:17:07 -0500

    http redirect to https

--- server.js
+++ server.js
@@ -1,33 +1,24 @@
 const https = require('https');
+const http = require('http');
 const path = require('path');
 const express = require('express');
 const app = express();
-const fs = require('fs')
+const fs = require('fs');
 
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-})
+});
 
 https.createServer({
   key: fs.readFileSync('data/server.key'),
   cert: fs.readFileSync('data/server.cert')}, 
   app).listen(443, () => {
   console.log('Listening...')
-})
+});
 
-// const http = require('http');
-// const path = require('path');
-// const express = require('express');
-// const app = express();
-
-// app.use(express.static(path.join(__dirname, 'build')));
-
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// })
-
-// http.createServer(app).listen(8080, () => {
-//   console.log('Listening...')
-// })
+http.createServer(function (req, res) {
+  res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+  res.end();
+}).listen(8080);
