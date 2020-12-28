56123a89d465bbd4450ef2cf739d773e4939ffe2
Jake Anstey <jake@Jakes-MacBook-Air.local>
2020-12-28 13:12:13 -0500

    port 80

--- server.js
+++ server.js
@@ -1,26 +1,8 @@
-const https = require('https');
-const path = require('path');
-const express = require('express');
-const app = express();
-const fs = require('fs')
-
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-})
-
-https.createServer({
-  key: fs.readFileSync('server.key'),
-  cert: fs.readFileSync('server.cert')}, 
-  app).listen(443, () => {
-  console.log('Listening...')
-})
-
-// const http = require('http');
+// const https = require('https');
 // const path = require('path');
 // const express = require('express');
 // const app = express();
+// const fs = require('fs')
 
 // app.use(express.static(path.join(__dirname, 'build')));
 
@@ -28,6 +10,24 @@ https.createServer({
 //   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 // })
 
-// http.createServer(app).listen(8080, () => {
+// https.createServer({
+//   key: fs.readFileSync('server.key'),
+//   cert: fs.readFileSync('server.cert')}, 
+//   app).listen(443, () => {
 //   console.log('Listening...')
 // })
+
+const http = require('http');
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
+
+http.createServer(app).listen(8080, () => {
+  console.log('Listening...')
+})
